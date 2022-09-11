a0dad1822e85c2a38d89bee89dc819c9cfa8ddfe
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-09-11 15:12:29 +0530

    Create findLCM.c

---
+++ Beginner Assignment/findLCM.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int findGCD(int a,int b){
+    if(a==0){
+        return b;
+    }
+    else{
+        return findGCD(b%a,a);
+    }
+}
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    
+    int gcd=findGCD(a,b);
+    int lcm=(a*b)/gcd;
+    printf("%d",lcm);
+    
+    return 0;
+}
