2240c396d185667cb3428de691f3af5b9ce87462
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-09-11 12:40:32 +0530

    Create bubbleSort.c

---
+++ Sorting Algorithms/bubbleSort.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        scanf("%d ",&arr[i]);
+    }
+    
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int t=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=t;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            printf("%d ",arr[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
