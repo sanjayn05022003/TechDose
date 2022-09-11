614378d7bfcf969e460f6ea0f642df205132b147
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-09-11 15:35:47 +0530

    Create addIntegerToDOuble.c

---
+++ Beginner Assignment/addIntegerToDOuble.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    double a;
+    scanf("%lf",&a);
+    
+    int num;
+    scanf("%d",&num);
+    
+    printf("%.2lf",a+num);
+
+    return 0;
+}
