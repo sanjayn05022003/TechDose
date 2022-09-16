cd90926414f543a22c699577546c64622460ef38
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-09-16 22:34:29 +0530

    Create radixSort.java

---
+++ Sorting Algorithms/radixSort.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Main
+{
+    public static void radixSort(int[] arr,int n,int place,int max){
+        
+        int[] ret=new int[n];
+        int[] count=new int[max+1];
+        
+        for(int i=0;i<n;i++){
+            count[arr[i]]++;
+        }
+        for(int i=1;i<=max;i++){
+            count[i]=count[i]+count[i-1];
+        }
+        
+        for(int i=0;i<n;i++){
+            ret[count[arr[i]]-1]=arr[i];
+            count[arr[i]]--;
+        }
+        
+        for(int i=0;i<n;i++){
+            arr[i]=ret[i];
+        }
+    }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		int[] arr=new int[n];
+	    int max=0;
+	    
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		    if(arr[i]>max){
+		        max=arr[i];
+		    }
+		}
+		
+		int place;
+		for(place=1;max/place>0;place*=10){
+		    radixSort(arr,n,place,max);
+		}
+	    
+	    for(int i=0;i<n;i++){
+	        System.out.print(arr[i]+" ");
+	    }
+	
+	}
+}
