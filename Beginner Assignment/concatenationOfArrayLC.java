049485c42a4fac83be5656b50c747edc7344ebc5
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-09-16 18:47:43 +0530

    Create concatenationOfArrayLC.java

---
+++ Beginner Assignment/concatenationOfArrayLC.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+        int len=nums.length;
+        int[] arr=new int[len*2];
+        
+        int fast=len;
+        
+        for(int i=0;i<len;i++){
+            arr[i]=nums[i];
+            arr[fast++]=nums[i];
+        }
+        return arr;
+        
+    }
+}
