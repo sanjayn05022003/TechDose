0135fb1b551cd514b7dbfa7f71f48cba25c7ad2b
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-09-29 18:15:55 +0530

    Create 121.BestTimeToBuyAndSellStock.java

---
+++ Leetcode Problems/121.BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int len=prices.length;
+        
+        int min=prices[0];
+        int diff=0;
+        
+        for(int i=1;i<len;i++){
+            if(prices[i]>min){
+                if((prices[i]-min)>diff){
+                    diff=prices[i]-min;
+                }
+            }
+            else{
+                min=prices[i];
+            }
+        }
+        
+        return diff;
+    }
+}
+Footer
+© 2022 Git
