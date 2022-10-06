0e89edeeee07d07a69c9035046e0959f5bb91fd5
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-10-06 10:05:19 +0530

    Delete 215.KthLargestElementInAnArray.java

--- Leetcode Problems/215.KthLargestElementInAnArray.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        
-        int len=nums.length;
-        Arrays.sort(nums);
-        
-        return nums[len-k];
-    }
-}
