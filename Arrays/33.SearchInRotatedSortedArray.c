9b713b8a3b8ce55f95978683c2fb373906a254a5
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-10-10 19:22:28 +0530

    Create 33.SearchInRotatedSortedArray.c

---
+++ Arrays/33.SearchInRotatedSortedArray.c
@@ -0,0 +1,12 @@
+int search(int* nums, int numsSize, int target){
+    
+    int ret=-1;
+    
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]==target){
+            ret=i;
+            break;
+        }
+    }
+    return ret;
+}
