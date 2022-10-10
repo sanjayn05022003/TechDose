0d017e1fe1aeafc12599a71dc47649acb2dd74e3
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-10-10 19:47:17 +0530

    Create 378.KthSmallestElementInASortedMatrix.java

---
+++ Arrays/378.KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k){
+        ArrayList<Integer>lst=new ArrayList<Integer>();
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                lst.add(matrix[i][j]);
+            }
+        }
+        Collections.sort(lst);
+        return lst.get(k-1);
+        
+    }
+}
