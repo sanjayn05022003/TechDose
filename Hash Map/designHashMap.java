a74dc4be08805c1c14bca2e2094c0d43f6f9a97d
SANJAY N <94184560+sanjayn05022003@users.noreply.github.com>
2022-10-27 11:49:13 +0530

    Create designHashMap.java

---
+++ Hash Map/designHashMap.java
@@ -0,0 +1,34 @@
+class MyHashMap {
+
+    int[] freq;
+
+    public MyHashMap() {
+         freq=new int[1000001];
+        Arrays.fill(freq,-1);
+    }
+    
+    public void put(int key, int value) {
+        freq[key]=value;
+    }
+    
+    public int get(int key) {
+        
+        if(freq[key]!=-1){
+            return freq[key];
+        }
+        return -1;
+        
+    }
+    
+    public void remove(int key) {
+        freq[key]=-1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
